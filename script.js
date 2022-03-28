4109f547a473daffc1751be2da075212fa895454
Marina Belova <salirsky@mail.ru>
2022-03-28 20:53:29 +0300

    Add prompt, confirm & switch

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 let title = "Practice";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 1200;
+let screens = prompt(
+  "Какие типы экранов нужно разработать?",
+  "Простые, Сложные, Интерактивные"
+);
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "16999");
 let rollback = 20;
-let fullPrice = 50740;
-let adaptive = true;
+
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+
+let service1 = prompt(
+  "Какой дополнительный тип услуги нужен?",
+  "Светлая/тёмная темы"
+);
+let servicePrice1 = +prompt("Сколько это будет стоить?", "8325");
+let service2 = prompt(
+  "Какой дополнительный тип услуги нужен?",
+  "Переключение языков ru/eng"
+);
+let servicePrice2 = +prompt("Сколько это будет стоить?", "7560");
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+// let servicePercentPrice2 = (fullPrice - fullPrice * (rollback / 100));
 
 console.log(typeof title);
 console.log(typeof fullPrice);
@@ -20,3 +39,23 @@ console.log(
     fullPrice * (rollback / 100) +
     " рублей"
 );
+
+console.log(servicePercentPrice);
+console.log(fullPrice);
+
+switch (true) {
+  case 30000 <= fullPrice:
+    console.log("Даем скидку в 10%");
+    break;
+  case 15000 <= fullPrice && fullPrice < 30000:
+    console.log("Даем скидку в 5%");
+    break;
+  case fullPrice < 15000:
+    console.log("Скидка не предусмотрена");
+    break;
+  case fullPrice < 0:
+    console.log("Что-то пошло не так");
+    break;
+  default:
+    console.log("Всё пошло вообще не так");
+}
