73be73323cc68a58cfe0c6d535241546db718973
Marina Belova <salirsky@mail.ru>
2022-03-29 19:57:27 +0300

    Adherence to code style

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+"Use strict";
+
 let title = prompt("Как называется ваш проект?");
-let screens = prompt(
+let screens = +prompt(
   "Какие типы экранов нужно разработать?",
   "Простые, Сложные, Интерактивные"
 );
@@ -24,29 +26,31 @@ const showTypeOf = function (variable) {
   console.log(variable, typeof variable);
 };
 
-switch (true) {
-  case 30000 <= fullPrice:
-    console.log("Даем скидку в 10%");
-    break;
-  case 15000 <= fullPrice && fullPrice < 30000:
-    console.log("Даем скидку в 5%");
-    break;
-  case fullPrice < 15000 && fullPrice == 0:
-    console.log("Скидка не предусмотрена");
-    break;
-  case fullPrice < 0:
-    console.log("Что-то пошло не так");
-    break;
-  default:
-    console.log("Вставай, Наташа х)");
-}
+const getRollbackMessage = function (price) {
+  switch (true) {
+    case 30000 <= price:
+      return "Даем скидку в 10%";
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
+    case 15000 <= price && price < 30000:
+      return "Даем скидку в 5%";
+
+    case price < 15000 && price == 0:
+      return "Скидка не предусмотрена";
+
+    case price < 0:
+      return "Что-то пошло не так";
+
+    default:
+      return "Вставай, Наташа х)";
+  }
+};
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
 console.log("Стоимость разработки сайта " + fullPrice + " рублей");
-console.log(screens.toLowerCase().split(", "));
 console.log(
   "Процент отката посреднику за работу " +
     fullPrice * (rollback / 100) +
